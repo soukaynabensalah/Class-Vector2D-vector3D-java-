0d515e956141199105a98319edb20185273feb38
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-11-27 07:37:06 +0100

    Create cl4.java

---
+++ cl4.java
@@ -0,0 +1,48 @@
+class Vector2D {
+    private int x;
+    private int y;
+    static int nv;
+    }
+    
+    public Vector2D {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public setX(int x){
+        this.x = x ;
+    }
+    
+    public setY(int y){
+        this.y = y;
+    }
+    
+    public string toSting {
+        return "x = " + x + "y = "+ y;
+    }
+    
+    public boolean Equals() {
+        if x==y {
+            return True;
+        }
+        
+    }
+    public int Norm() {
+        return ( x ** 2 + y ** 2 ) ** 1/2;
+    }
+    
+    public class Vector3D extends Vector3D {
+        public Vector3D (int x int y int z) {
+            super(x, y, z)
+        }
+    }
+        
+    
